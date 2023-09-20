bbe422a70ea67fbe6ad0be777d0e06af9cada5d6
IanniMuliterno <iannimuliterno@gmail.com>
2023-09-20 12:31:27 -0300

    ultima atualização

--- draft.R
+++ draft.R
@@ -28,3 +28,97 @@ ggplot(repeated_guests, aes(x = type, y = n, fill = type)) +
        y = "Número de Hóspedes") +
   theme_minimal() +
   theme(legend.position = "none")
+
+
+
+###################################
+# Carregue os pacotes necessários
+library(dplyr)
+library(tidyr)
+library(fmsb)
+
+df_processed <- df_hotels %>%
+  filter(is_canceled == 0) %>%
+  mutate(
+    parents_detailed = case_when(children > 0 ~ "pais",
+                                 babies > 0 ~ "pais_jovens",
+                                 TRUE ~ "sem_filhos"),
+    stay_duration = stays_in_weekend_nights + stays_in_week_nights,
+    special_requests = if_else(total_of_special_requests > 0, 1, 0)
+  ) %>%
+  group_by(parents_detailed) %>%
+  summarize(
+    stay_duration = mean(stay_duration, na.rm = TRUE),
+    adr = mean(adr, na.rm = TRUE),
+    special_requests = mean(special_requests, na.rm = TRUE),
+    meal_BB = mean(meal == "BB", na.rm = TRUE)
+  ) %>%
+  ungroup()
+
+# Identificar valores máximos e mínimos
+max_values <- df_processed %>%
+  summarise(across(everything(), max, na.rm = TRUE))
+
+min_values <- df_processed %>%
+  summarise(across(everything(), min, na.rm = TRUE))
+
+# Renomeando as linhas para aderir ao formato esperado
+rownames(max_values) <- "max"
+rownames(min_values) <- "min"
+df_radar <- rbind(max_values, min_values, df_processed)
+
+
+radarchart(df_radar[,-1], seg=4,
+           axistype = 1, 
+           #pcol = c("pais" = "#FC4E07", "pais_jovens" = "#E7B800", "sem_filhos" = "#00AFBB", "min" = "#FFFFFF", "max" = "#FFFFFF"), 
+           plwd = 1,
+           plty = 1)
+
+
+radarchart(dat, axistype=1, seg=5, plty=1, vlabels=c("Total\nQOL", "Physical\naspects", 
+                                                     "Phychological\naspects", "Social\naspects", "Environmental\naspects"), 
+           title="(axis=1, 5 segments, with specified vlabels)", vlcex=0.5)
+
+
+
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+# Transformação dos dados
+df_long <- df_processed %>%
+  pivot_longer(cols = -parents_detailed, names_to = "variable", values_to = "value")
+
+# Desenhar o gráfico de radar
+ggplot(df_long, aes(x = variable, y = value, group = parents_detailed, color = parents_detailed)) +
+  geom_polygon(fill = NA, size = 1.5) + 
+  geom_line(size = 1) +
+  coord_polar() +
+  theme_minimal() +
+  labs(title = "Gráfico de Radar para Características de Hospedagem") +
+  theme(legend.position = "bottom")
+
+
+
+
+# Calculando o valor máximo dos dados
+max_value <- max(df_radar[,-1], na.rm = TRUE)
+
+# Ajustando os dados para o gráfico de radar
+df_radar <- df_long %>%
+  group_by(parents_detailed, variable) %>%
+  summarize(value = mean(value, na.rm = TRUE)) %>%
+  spread(key = variable, value = value) %>%
+  arrange(desc(parents_detailed))
+
+# Normalizando os dados para valores entre 0 e 1
+normalize <- function(x) {
+  return ((x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE)))
+}
+
+df_normalized <- df_radar
+df_normalized[,-1] <- as.data.frame(lapply(df_normalized[,-1], normalize))
+
+# Gráfico de radar com dados normalizados
+ggradar(df_normalized, grid.max = 1) + 
+  scale_fill_manual(values = c("pais" = "#FC4E07", "pais_jovens" = "#E7B800", "sem_filhos" = "#00AFBB"))
--- script.R
+++ script.R
@@ -4,6 +4,8 @@ library(GGally)
 library(tidymodels)
 library(recipes)
 library(workflows)
+library(RColorBrewer)
+library(ggcorrplot)
 #funções
 # Função para plotar histogramas em massa para variáveis numéricas
 plot_histograms <- function(data) {
@@ -153,7 +155,7 @@ df_processed <- df_hotels |>
   filter(group == "yes")
 
 # Paletas de cores simplificadas
-color_fill <- c("young_parents" = "#66c2a5", "parents" = "#fc8d62", "no_parents" = "#8da0cb")
+color_fill <- c("pais_jovens" = "#66c2a5", "pais" = "#fc8d62", "sem_filhos" = "#8da0cb")
 
   ggplot(df_processed , aes(x = variable, y = value, fill = parents_detailed)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -165,16 +167,52 @@ color_fill <- c("young_parents" = "#66c2a5", "parents" = "#fc8d62", "no_parents"
        fill = "tem filhos?",
        alpha = "Type of Parent") +
   theme_minimal()
-p
+# stays , previous, adr
+correlacao <-   hotel_stays |> 
+    select(starts_with('stays'),starts_with('previous'),adr) |> 
+  janitor::clean_names() |>  
+  cor()   
+    corrplot::corrplot(correlacao, tl.col = "red", bg = "White", tl.srt = 45,
+                        addCoef.col = "black", type = "lower")
 
+# mas...
+
+    
+ # Dividir as variáveis pelo tipo
+numeric_vars <- names(hotel_stays)[sapply(hotel_stays, is.numeric)]
+categorical_vars <- names(hotel_stays)[sapply(hotel_stays, is.character)]
+
+# Correlação de Pearson para numéricas
+numeric_cor <- cor(hotel_stays[, numeric_vars], method = "pearson")
+    
+# Correlação de Spearman para ordinais (usando numéricas como exemplo)
+ordinal_cor <- cor(hotel_stays[, numeric_vars], method = "spearman")
+    
+# Qui-quadrado para categóricas
+chi_results <- list()
+for(i in 1:(length(categorical_vars)-1)) {
+  for(j in (i+1):length(categorical_vars)) {
+    var1 <- as.factor(hotel_stays[[categorical_vars[i]]])
+    var2 <- as.factor(hotel_stays[[categorical_vars[j]]])
+    chi_table <- table(var1, var2)
+    chi_results[[paste0(categorical_vars[i], " vs ", categorical_vars[j])]] <- chisq.test(chi_table)
+  }
+}
+    
+# Mostrar resultados
+    numeric_cor
+    ordinal_cor
+    chi_results
+    
+  
 # GGPAIRS
 
-hotel_stays %>%
+hotel_stays |> 
   select(
     children, adr,
     required_car_parking_spaces,
     total_of_special_requests
-  ) %>%
+  ) |> 
   ggpairs(mapping = aes(color = children))
 # 6. Modelagem e Machine Learning
 # 
@@ -182,49 +220,114 @@ hotel_stays %>%
 # Árvores de decisão e florestas aleatórias
 # Validando e avaliando modelos
 
-hotels_df <- hotel_stays %>%
+hotels_df <- hotel_stays |> 
   select(
     children, hotel, arrival_date_month, meal, adr, adults,
     required_car_parking_spaces, total_of_special_requests,
     stays_in_week_nights, stays_in_weekend_nights
-  ) %>%
+  ) |> 
   mutate_if(is.character, factor)
 
+#### Data prep manual
+set.seed(2023)
 
+# Divisão de Dados
+hotel_split <- initial_split(hotels_df)
+hotel_train <- training(hotel_split)
+hotel_test <- testing(hotel_split)
+
+# Downsampling
+minority_class_size <- sum(hotel_train$children == "children")
+majority_class_indices <- which(hotel_train$children == "none")
+selected_majority_indices <- sample(majority_class_indices, minority_class_size)
+downsampled_indices <- c(which(hotel_train$children == "children"), selected_majority_indices)
+hotel_train_downsampled <- hotel_train[downsampled_indices, ]
+
+# Separar colunas numéricas e categóricas
+num_vars <- names(hotel_train_downsampled)[sapply(hotel_train_downsampled, is.numeric)]
+cat_vars <- setdiff(names(hotel_train_downsampled), c(num_vars, "children"))
+
+# Criar dummies para as colunas categóricas
+dummy_train_cat <- model.matrix(~ . + 0, data = hotel_train_downsampled[, cat_vars, drop=FALSE])
+dummy_test_cat <- model.matrix(~ . + 0, data = hotel_test[, cat_vars, drop=FALSE])
+
+# Remover colunas com Variância Zero nas dummies categóricas
+zv_cols_cat <- which(apply(dummy_train_cat, 2, var) == 0)
+dummy_train_cat <- dummy_train_cat[, -zv_cols_cat]
+dummy_test_cat <- dummy_test_cat[, -zv_cols_cat]
+
+# Normalizar as colunas numéricas
+mean_train_num <- apply(hotel_train_downsampled[, num_vars, drop=FALSE], 2, mean)
+std_train_num <- apply(hotel_train_downsampled[, num_vars, drop=FALSE], 2, sd)
+
+#sweep:
+# A função sweep é usada para "varrer" uma matriz ou array por suas margens. No contexto do código, ela é usada para subtrair a média (mean_train_num) e depois dividir pelo desvio padrão (std_train_num) para cada coluna. Esta é a operação de normalização.
+# 
+# O primeiro argumento é a matriz/array.
+# O segundo argumento é a margem (1 = linhas, 2 = colunas).
+# O terceiro argumento é o vetor de estatísticas (média ou desvio padrão).
+# O quarto argumento é a função a ser aplicada (FUN = "-" para subtração e FUN = "/" para divisão).
+
+hotel_train_norm <- sweep(hotel_train_downsampled[, num_vars, drop=FALSE], 2, mean_train_num, FUN = "-")
+hotel_train_norm <- sweep(hotel_train_norm, 2, std_train_num, FUN = "/")
+
+hotel_test_norm <- sweep(hotel_test[, num_vars, drop=FALSE], 2, mean_train_num, FUN = "-")
+hotel_test_norm <- sweep(hotel_test_norm, 2, std_train_num, FUN = "/")
+
+# Juntar dados normalizados e variáveis dummy
+dummy_train <- cbind(dummy_train_cat, hotel_train_norm)
+dummy_test <- cbind(dummy_test_cat, hotel_test_norm)
+
+# Resultado Final
+test_proc <- as.data.frame(dummy_test)
+
+
+#### Como fica com recipes
 set.seed(2023)
 hotel_split <- initial_split(hotels_df)
 
 hotel_train <- training(hotel_split)
 hotel_test <- testing(hotel_split)
 
-hotel_rec <- recipe(children ~ ., data = hotel_train) %>%
-  themis::step_downsample(children) %>%
-  step_dummy(all_nominal(), -all_outcomes()) %>%
-  step_zv(all_numeric()) %>%
-  step_normalize(all_numeric()) %>%
+hotel_rec <- recipe(children ~ ., data = hotel_train) |> 
+  themis::step_downsample(children) |> 
+  step_dummy(all_nominal(), -all_outcomes()) |> 
+  step_zv(all_numeric()) |> 
+  step_normalize(all_numeric()) |> 
   prep()
 
 hotel_rec
 
 test_proc <- bake(hotel_rec, new_data = hotel_test)
 
-knn_spec <- nearest_neighbor() %>%
-  set_engine("kknn") %>%
+knn_spec <- nearest_neighbor() |> 
+  set_engine("kknn") |> 
   set_mode("classification")
 
-knn_fit <- knn_spec %>%
+knn_fit <- knn_spec |> 
   fit(children ~ ., data = juice(hotel_rec))
 
 knn_fit
 
-tree_spec <- decision_tree() %>%
-  set_engine("rpart") %>%
+tree_spec <- decision_tree() |> 
+  set_engine("rpart") |> 
   set_mode("classification")
 
-tree_fit <- tree_spec %>%
+tree_fit <- tree_spec |> 
   fit(children ~ ., data = juice(hotel_rec))
 
 tree_fit
+
+
+# Especificar o modelo GLM
+glm_spec <- logistic_reg() |>  
+  set_engine("glm") |>  
+  set_mode("classification")
+
+# Ajuste o modelo
+glm_fit <- glm_spec |>  
+  fit(children ~ ., data = juice(hotel_rec))
+glm_fit
 # 7. Comunicação dos Resultados
 # (falar sobre)
 # Como apresentar insights
@@ -234,6 +337,11 @@ set.seed(1234)
 validation_splits <- mc_cv(juice(hotel_rec), prop = 0.9, strata = children)
 validation_splits
 
+# Definir o fluxo de trabalho para o GLM
+glm_wf <- workflow() |>  
+  add_model(glm_spec) |>  
+  add_formula(children ~ .)
+
 knn_wf <- workflow() |> 
   add_model(knn_spec) |> 
   add_formula(children ~.)
@@ -241,6 +349,9 @@ knn_wf <- workflow() |>
 tree_wf <- workflow() |> 
   add_model(tree_spec) |> 
   add_formula(children ~.)
+
+# Ajustar o modelo usando validação cruzada
+
 
 knn_res <- fit_resamples(
   knn_wf,
@@ -254,20 +365,37 @@ tree_res <- fit_resamples(
   control = control_resamples(save_pred = TRUE)
 )
 
-tree_res %>%
+
+glm_res <- fit_resamples(
+  glm_wf,
+  validation_splits,
+  control = control_resamples(save_pred = TRUE)
+)
+
+
+# Coletar métricas
+
+tree_res |> 
   collect_metrics()
 
-knn_res %>%
+knn_res |> 
   collect_metrics()
 
-knn_res %>%
-  unnest(.predictions) %>%
-  mutate(model = "kknn") %>%
-  bind_rows(tree_res %>%
-              unnest(.predictions) %>%
-              mutate(model = "rpart")) %>%
-  group_by(model) %>%
-  roc_curve(children, .pred_children) %>%
+glm_res |>  
+  collect_metrics()
+
+
+knn_res |> 
+  unnest(.predictions) |> 
+  mutate(model = "kknn") |> 
+  bind_rows(tree_res |> 
+              unnest(.predictions) |> 
+              mutate(model = "rpart")) |> 
+  bind_rows(glm_res |> 
+              unnest(.predictions) |> 
+              mutate(model = "glm")) |> 
+  group_by(model) |> 
+  roc_curve(children, .pred_children) |> 
   ggplot(aes(x = 1 - specificity, y = sensitivity, color = model)) +
   geom_line(size = 1.5) +
   geom_abline(
@@ -276,8 +404,20 @@ knn_res %>%
     size = 1.2
   )
 
+glm_fit |> 
+  predict(new_data = test_proc, type = "prob") |> 
+  mutate(truth = hotel_test$children) |> 
+  roc_auc(truth, .pred_children)
+##################
+# provando que o recipes pode pre processar para que vc use em modelos comuns
+# Definindo a receita
+rec <- recipe(mpg ~ ., data = mtcars)  |> 
+  step_center(all_predictors())  |> 
+  step_scale(all_predictors())
 
-# 9. Ética em Ciência de Dados
-#  ( se sobrer tempo, poderiamos falar de)
-# Privacidade e segurança dos dados
-# Bias e equidade em modelagem
+rec_prepared <- prep(rec, training = mtcars)
+
+data_transformed <- bake(rec_prepared, new_data = mtcars)
+
+model <- lm(mpg ~ ., data = data_transformed)
+summary(model)
